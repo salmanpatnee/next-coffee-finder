3dd445e5253af3f9378e63954019c60304c4ff7b
salmanpatnee <salmanpatni92@gmail.com>
2024-12-17 13:36:50 +0500

    Set up Radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Theme} from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme accentColor="gold">
+          <main>
+            {children}
+          </main>
+        </Theme>
       </body>
     </html>
   );
