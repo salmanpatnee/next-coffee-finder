6b0253841399c32753fa6765d1b16ca27fdf1ba1
salmanpatnee <salmanpatni92@gmail.com>
2024-12-17 18:19:07 +0500

    Add banner

---
+++ app/components/Banner.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Button, Container, Flex, Grid, Heading, Text } from '@radix-ui/themes'
+import Image from 'next/image';
+import React from 'react'
+
+interface Props {
+  onButtonClick: () => void; 
+}
+
+const Banner = ({onButtonClick}: Props) => {
+
+  return (
+    <section>
+      <Container>
+        <Grid columns={{initial: "1", sm: "2"}} gap="5" align={'center'}>
+          <Flex direction="column"  gap="5">
+            <Heading className='text-white' size={'9'} weight={'bold'}>Find Your Perfect Coffee</Heading>
+            <Text  as='p' size={'5'} className='text-white'>Explore a wide range of coffees, from bold brews to smooth blends, tailored just for you.</Text>
+            <Button size={'4'} variant='surface' onClick={onButtonClick}>Start Your Search</Button>
+          </Flex>
+          <Flex>
+            <Image src="/images/coffee-beans.png" alt="Coffee beans" width={500} height={500}/>
+          </Flex>
+        </Grid>
+      </Container>
+    </section>
+  )
+}
+
+export default Banner
